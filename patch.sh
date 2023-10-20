774122972177c6c36727cf10e912e8594daa7cb6
Travis Johnson <travis@thisguy.codes>
2023-10-20 01:02:45 -0400

    derp

--- patch.sh
+++ patch.sh
@@ -7,7 +7,7 @@ rm -rf testing.go *_test.go fuzzy
 cp ../patcher/new_files/* .
 
 go install golang.org/x/tools/cmd/goimports@latest
-goimports -w -local 'github.com/hashicorp'
+goimports -w -local 'github.com/hashicorp' .
 
 go mod edit -module github.com/thisguycodes/raft
 go mod edit -go 1.21
